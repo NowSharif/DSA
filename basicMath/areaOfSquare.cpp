9e902d7f97c6623a1c888b799956a547498d03bf
NowSharif <sharifshaik1818@gmail.com>
2023-09-09 09:55:29 +0530

    Added areaOfSquare.cpp in basicMath

---
+++ basicMath/areaOfSquare.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int side;
+  cout<< "Enter the side of the square"<<endl;
+  cin >> side;
+
+  int areaOfSquare = side*side;
+
+  cout << "Area of the square is: " << areaOfSquare <<endl;
+
+return 0;
+}
+
+
+/*
+
+Output:
+
+Enter the side of the square:
+>> 23
+Area of the square is: 529
+
+*/
