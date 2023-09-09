365baefd3a31408cdb3f02613d984d960e087587
NowSharif <sharifshaik1818@gmail.com>
2023-09-09 09:22:15 +0530

    basicMath folder added

---
+++ basicMath/helloUniverse.cpp
@@ -0,0 +1,22 @@
+
+//Program to print Hello Universe!
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    cout << "Hello Universe!";
+
+    return 0;
+}
+
+
+
+/* 
+
+Ouput:
+
+Hello Universe!
+
+*/
