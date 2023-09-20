87aa8065daf5978d55129e79653fac627a463b5b
NowSharif <sharifshaik1818@gmail.com>
2023-09-20 13:16:59 +0530

    Updated week1Debug8.cpp

--- Practice/week1Debug8.cpp
+++ Practice/week1Debug8.cpp
@@ -13,19 +13,30 @@ N = 5
 using namespace std;
 
 
+/*
+Pattern
+N = 5
+  *
+ ***
+*****
+ ***
+  *
+*/
+
+
 void printPatt(int n){
     int i=1;
-    while(i<(n)){
-        int gaps = n-2*i-1,k=1;
-        if(i>(n)/2){
+    while(i<=(n)){
+        int gaps = n-2*i+1,k=1;
+        if(i>(n+1)/2){
             int no = (n+1)/2;
-            gaps = n-2*(i%no)-1;
+            gaps = 2*(i%no);
         }
         while(k<=gaps/2){
             cout<<" ";
             k = k + 1;
         }
-        int ch = n-1-gaps;
+        int ch = n - gaps;
         while(ch>=1){
             cout<<"*";
             ch = ch - 1;
@@ -39,7 +50,6 @@ void printPatt(int n){
         i = i + 1;
     }
 }
-
 
 
 int main(){
