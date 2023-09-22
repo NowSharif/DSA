5fcaaa5b94d0522aa7a0d0d7d084477113178cff
NowSharif <sharifshaik1818@gmail.com>
2023-09-22 17:41:49 +0530

    Updated findQuotient.cpp

--- SearchingANDSorting/findQuotient.cpp
+++ SearchingANDSorting/findQuotient.cpp
@@ -1,23 +1,32 @@
 //Divide 2 numbers using binary search
+// 29. Divide Two Integers
+//https://leetcode.com/problems/divide-two-integers/
+
+//TC = O(log(Dividend)) ; SC=O(1)
 
 #include <iostream>
 #include <vector>
+#include <limits.h>
 
 using namespace std;
 
 
 int getQuotient(int dividend, int divisor){
 
-    int ans = -1;
+    long long int ans = -1;
 
-    int start = 0, end = dividend;
-    int mid = start + (end-start)/2;
+    long long int dividend2 = (long long int)(dividend);
+    long long int divisor2 = (long long int)(divisor);
+
+    long long int start = 0, end = dividend2;
+    long long int mid = start + (end-start)/2;
 
     while(start<=end){
-        if(mid*divisor==dividend){
+       
+        if(mid*divisor2==dividend2){
             return mid;
         }
-        else if(mid*divisor < dividend){
+        else if(mid*divisor2 < dividend2){
             ans = mid;
             start = mid+1;
         }
@@ -30,20 +39,33 @@ int getQuotient(int dividend, int divisor){
 
     }
 
-
-    return ans;
-
-
-
     return ans;
 }
 
 int main(){
 
-    int dividend = -79;
-    int divisor = -8;
+    // long int dividend = -2147483648;
+    // long int divisor = -1;
 
-    int quotient = getQuotient(abs(dividend), abs(divisor));
+    
+    long int dividend = 2147483647;
+    int divisor = 2;
+
+    long int quotient = -1;
+    
+    if(dividend<=INT_MIN && divisor==-1){
+        quotient =  INT_MAX;
+    }
+
+     if(dividend<=INT_MIN && divisor==1){
+        quotient =  INT_MIN;
+    }
+
+   
+    if(quotient ==-1)
+    quotient = getQuotient(abs(dividend), abs(divisor));
+
+     
 
     if(quotient!=-1){
         if((divisor>0 && dividend<0) || (divisor<0 && dividend>0)){
